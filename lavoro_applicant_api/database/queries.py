dba6af7e53f430099ff96077ca1bbc62170cadc3
Maksim <madzarmaksim@gmail.com>
2023-11-13 14:31:47 +0100

    refactor

--- lavoro_applicant_api/database/queries.py
+++ lavoro_applicant_api/database/queries.py
@@ -1,25 +1,7 @@
-from enum import Enum
-import json
-from uuid import UUID
-
 from lavoro_applicant_api.database import db
 from lavoro_library.models import ApplicantProfileDto, CreateApplicantProfileRequest, ApplicantProfile, Experience, \
     ExperienceDto, Point
-
-
-INSERT_APPLICANT_PROFILE_SQL = '''
-    INSERT INTO applicant_profiles (
-        first_name, last_name, education_level_id, age, gender, skills_id, 
-        account_id, cv_url, work_type_id, seniority_level, position_id, 
-        home_location, work_location_max_distance, contract_type_id, min_salary
-    ) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s) RETURNING id
-'''
-
-
-INSERT_EXPERIENCE_SQL = '''
-    INSERT INTO experiences (company_name, position_id, years, applicant_profile_id)
-    VALUES (%s, %s, %s, %s) RETURNING id
-'''
+from lavoro_applicant_api.helpers.data_helpers import prepare_applicant_profile_data, prepare_experience_data, extract_id
 
 
 def create_applicant_profile(form_data: CreateApplicantProfileRequest):
@@ -39,46 +21,3 @@ def insert_experiences(experiences, applicant_profile_id):
     db.execute_many(query_tuples)
 
 
-def prepare_applicant_profile_data(form_data: CreateApplicantProfileRequest):
-    return (
-        INSERT_APPLICANT_PROFILE_SQL,
-        (
-            form_data.first_name, form_data.last_name,
-            form_data.education_level_id, form_data.age, convert_value(form_data.gender),
-            form_data.skills_id, convert_value(form_data.account_id), form_data.cv_url,
-            form_data.work_type_id, form_data.seniority_level,
-            form_data.position_id, convert_value(form_data.home_location),
-            form_data.work_location_max_distance, form_data.contract_type_id,
-            form_data.min_salary
-         )
-    )
-
-
-def prepare_experience_data(experience, applicant_profile_id):
-    return (
-        INSERT_EXPERIENCE_SQL,
-        (
-            experience.company_name,
-            experience.position_id,
-            experience.years,
-            applicant_profile_id
-        )
-    )
-
-
-def extract_id(result):
-    return result["result"][0]["id"] if result["result"] else None
-
-
-def convert_value(value):
-    if isinstance(value, UUID):
-        return str(value)
-    elif isinstance(value, Enum):
-        return value.value
-    elif isinstance(value, dict):
-        if 'x' in value and 'y' in value:
-            return f"({value['x']}, {value['y']})"
-        else:
-            return json.dumps(value)
-    else:
-        return value
---
+++ lavoro_applicant_api/helpers/data_helpers.py
@@ -0,0 +1,65 @@
+from enum import Enum
+import json
+from uuid import UUID
+from lavoro_library.models import CreateApplicantProfileRequest
+
+
+INSERT_APPLICANT_PROFILE_SQL = '''
+    INSERT INTO applicant_profiles (
+        first_name, last_name, education_level_id, age, gender, skills_id, 
+        account_id, cv_url, work_type_id, seniority_level, position_id, 
+        home_location, work_location_max_distance, contract_type_id, min_salary
+    ) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s) RETURNING id
+'''
+
+
+INSERT_EXPERIENCE_SQL = '''
+    INSERT INTO experiences (company_name, position_id, years, applicant_profile_id)
+    VALUES (%s, %s, %s, %s) RETURNING id
+'''
+
+
+def prepare_applicant_profile_data(form_data: CreateApplicantProfileRequest):
+    return (
+        INSERT_APPLICANT_PROFILE_SQL,
+        (
+            form_data.first_name, form_data.last_name,
+            form_data.education_level_id, form_data.age, convert_value(form_data.gender),
+            form_data.skills_id, convert_value(form_data.account_id), form_data.cv_url,
+            form_data.work_type_id, form_data.seniority_level,
+            form_data.position_id, convert_value(form_data.home_location),
+            form_data.work_location_max_distance, form_data.contract_type_id,
+            form_data.min_salary
+         )
+    )
+
+
+def prepare_experience_data(experience, applicant_profile_id):
+    return (
+        INSERT_EXPERIENCE_SQL,
+        (
+            experience.company_name,
+            experience.position_id,
+            experience.years,
+            applicant_profile_id
+        )
+    )
+
+
+def extract_id(result):
+    return result["result"][0]["id"] if result["result"] else None
+
+
+def convert_value(value):
+    if isinstance(value, UUID):
+        return str(value)
+    elif isinstance(value, Enum):
+        return value.value
+    elif isinstance(value, dict):
+        if 'x' in value and 'y' in value:
+            return f"({value['x']}, {value['y']})"
+        else:
+            return json.dumps(value)
+    else:
+        return value
+
