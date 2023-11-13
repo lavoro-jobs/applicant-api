2cf305e9c431f8c9266049b4e3700610708a580c
Maksim <madzarmaksim@gmail.com>
2023-11-13 14:33:27 +0100

    remove unnecessary uri prefix

--- lavoro_applicant_api/applicant_api.py
+++ lavoro_applicant_api/applicant_api.py
@@ -4,7 +4,7 @@ from lavoro_applicant_api.database import db
 from lavoro_applicant_api.routers.applicant import router as applicant_router
 
 
-router = APIRouter(prefix="/api/v1")
+router = APIRouter()
 router.include_router(applicant_router)
 
 app = FastAPI()
